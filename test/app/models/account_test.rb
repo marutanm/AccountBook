7704518f1e4a292d7050202a421c3727f1efe991
marutanm <marutanm@gmail.com>
2011-06-02 22:53:03 +0900

    test for account model

--- test/app/models/account_test.rb
+++ test/app/models/account_test.rb
@@ -1,17 +1,21 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../test_config.rb')
 
 class AccountTest < Test::Unit::TestCase
-  context "Account Model" do
-    should 'construct new instance' do
-      @account = Account.new
+  context "Account Model create new document" do
+    setup do
+      @auth = {"uid" => "100", "name" => "testuser"}
+      @account = Account.create_with_omniauth(@auth)
+    end
+
+    should "return created document" do
       assert_not_nil @account
+      assert_equal @auth["uid"], @account.uid
+      assert_equal @auth["name"], @account.name
+      assert_equal "users", @account.role
     end
-    should 'create with omniauth' do
-      @auth = {:uid => "100", :name => "name"}
-      assert_not_nil Account.create_with_omniauth(@auth)
-    end
-    should 'find by id' do
-      assert_nil Account.find_by_id(:hoge)
+
+    teardown do
+      Account.delete_all
     end
   end
 end
