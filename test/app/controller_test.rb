39a413a688d74d06acb511ebb855118918223a34
marutanm <marutanm@gmail.com>
2011-06-02 12:52:27 +0900

    use Rack::Test

--- test/app/controller_test.rb
+++ test/app/controller_test.rb
@@ -1,13 +1,27 @@
 require File.expand_path(File.dirname(__FILE__) + '/../test_config.rb')
 
 class ControllerTest < Test::Unit::TestCase
-  context "Contoller" do
+  include Rack::Test::Methods
+
+  context "GET '/'" do
     setup do
       get '/'
     end
 
     should "return hello world text" do
+      assert last_response.ok?
       assert_equal "Hello world!", last_response.body
     end
   end
+
+  context "GET unknown path" do
+    setup do
+      get '/unknown'
+    end
+    should "return 404" do
+      assert !last_response.ok?
+      assert_equal 404, last_response.status
+    end
+  end
+
 end
