72998f3f8de4494ca39111bf9c87611b2b76d1e9
marutanm <marutanm@gmail.com>
2011-06-04 00:10:58 +0900

    test for find_or_create_with_omniauth

--- test/app/models/account_test.rb
+++ test/app/models/account_test.rb
@@ -1,21 +1,56 @@
 require File.expand_path(File.dirname(__FILE__) + '/../../test_config.rb')
 
 class AccountTest < Test::Unit::TestCase
-  context "Account Model create new document" do
-    setup do
-      @auth = {"uid" => "100", "name" => "testuser"}
-      @account = Account.create_with_omniauth(@auth)
+  context "Account Model" do
+
+    context "create_with_omniauth" do
+      setup do
+        @auth = {"uid" => "100", "name" => "testuser"}
+        @account = Account.create_with_omniauth(@auth)
+      end
+      should "return created document" do
+        assert_equal Account, @account.class
+        assert_equal @auth["uid"], @account.uid
+        assert_equal @auth["name"], @account.name
+        assert_equal "users", @account.role
+      end
+      teardown do
+        Account.delete_all
+      end
     end
 
-    should "return created document" do
-      assert_not_nil @account
-      assert_equal @auth["uid"], @account.uid
-      assert_equal @auth["name"], @account.name
-      assert_equal "users", @account.role
+    context "find_or_create_with_omniauth" do
+      context "account not registerd" do
+        setup do
+          @auth = {"uid" => "100", "name" => "testuser"}
+          @account = Account.find_or_create_with_omniauth(@auth)
+        end
+        should "create and return document" do
+          assert_equal Account, @account.class
+          assert_equal @auth["uid"], @account.uid
+          assert_equal @auth["name"], @account.name
+          assert_equal "users", @account.role
+        end
+      end
+      context "account already registerd" do
+        setup do 
+          @auth = {"uid" => "100", "name" => "testuser"}
+          Account.create_with_omniauth(@auth)
+          @auth = {"uid" => "100", "name" => "modifieduesr"}
+          @account = Account.find_or_create_with_omniauth(@auth)
+        end
+        should "find and return exsitance document" do
+          assert_equal Account, @account.class
+          assert_equal @auth["uid"], @account.uid
+          assert_equal @auth["name"], @account.name
+          assert_equal "users", @account.role
+          assert_equal 2, Account.count
+        end
+        teardown do
+          Account.delete_all
+        end
+      end
     end
 
-    teardown do
-      Account.delete_all
-    end
   end
 end
