f1c189dadb8e6ce35287f92a811929babbb7c859
marutanm <marutanm@gmail.com>
2011-06-09 13:05:00 +0900

    test code for create_new_document of Spend model

--- test/app/models/spend_test.rb
+++ test/app/models/spend_test.rb
@@ -2,9 +2,40 @@ require File.expand_path(File.dirname(__FILE__) + '/../../test_config.rb')
 
 class SpendTest < Test::Unit::TestCase
   context "Spend Model" do
-    should 'construct new instance' do
-      @spend = Spend.new
-      assert_not_nil @spend
+    context "create_new_document" do
+      context "spend" do
+        setup do
+          @params = {:uid => "100", 
+            :amount => 1000,
+            :comment => "this is comment",
+            :category => "expend"}
+          @spend = Spend.create_new_document(@params)
+        end
+        should "return created document" do
+          assert_equal @params[:uid], @spend.uid
+          assert_equal @params[:amount], @spend.amount
+          assert_equal @params[:comment], @spend.comment
+          assert @spend.isSpend
+        end
+      end
+
+      context "renew current balance" do
+        setup do
+          @params = {:uid => "100", 
+            :amount => 1000,
+            :comment => "this is comment",
+            :category => "current"}
+          @spend = Spend.create_new_document(@params)
+        end
+        should "return created document" do
+          assert_equal @params[:uid], @spend.uid
+          assert_equal @params[:amount], @spend.amount
+          assert_equal @params[:amount], @spend.current
+          assert_equal @params[:comment], @spend.comment
+          assert !@spend.isSpend
+        end
+      end
+
     end
   end
 end
